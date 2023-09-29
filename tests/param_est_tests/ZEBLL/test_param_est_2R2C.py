22ba74e0408a76adf608fe7b4da51f875a9e4de0
Marius Bagle <marius@NTNU24923>
2023-09-29 08:26:28 +0200

    small change in test script to test submodule workings

--- tests/param_est_tests/ZEBLL/test_param_est_2R2C.py
+++ tests/param_est_tests/ZEBLL/test_param_est_2R2C.py
@@ -22,14 +22,14 @@ import os
 rc('mathtext', default='regular')
 # datetime:
 #plt.rcParams["date.autoformatter.minute"] = "%Y-%m-%d %H:%M"
-import matplotlib.dates as mdates
+#import matplotlib.dates as mdates
 
 if __name__ == "__main__":
-    
+
     cfg_path = os.path.join("configs", "2R2C.json")
     data_path = os.path.join(get_data_path(), "data_ZEBLL_PRBS.csv")
     ekf_config = os.path.join(get_opt_config_path(), "2R2C_EKF.json")
-    
+
     data = pd.read_csv(data_path)
     # dt is 30 seconds:
     data.index = pd.TimedeltaIndex(data["Unnamed: 0"])
@@ -41,9 +41,9 @@ if __name__ == "__main__":
     downsample = True
     sampling_time = "15min"
     dt = 900
-    
+
     if downsample:
-        
+
         heat = data[["phi_h", "phi_s"]]
         heat = heat.resample(sampling_time).mean()
 
@@ -53,13 +53,13 @@ if __name__ == "__main__":
         y_data = pd.merge(heat, rest, left_index=True, right_index=True)
         y_data["y1"] = y_data.Ti
         y_data.index = np.arange(0, len(y_data)*900, 900)
-        
+
     else:
-        
+
         y_data = data
         y_data["y1"] = y_data.Ti
         y_data.index = np.arange(0, len(y_data)*30, 30)
-    
+
     """
     fig, ax = plt.subplots(2,1)
     (y_data.Ti-273.15).plot(color="k", ax=ax[0])
@@ -69,28 +69,28 @@ if __name__ == "__main__":
     fig.tight_layout()
     plt.show()
     """
-    
+
     #y_data = y_data.iloc[0:3]
-    
+
     N = len(y_data)
     dt = y_data.index[1] - y_data.index[0]
-    
+
     param_guess = ca.DM([0.01,0.1,1E6,1E7,2])
     #param_guess = ca.DM([1.532625e-03,1.160423e-02,1.328633e+06,6.644898e+06,5.578587e+00])
     lbp = ca.DM([0.001,0.01,1E5,1E6,1])
     ubp = ca.DM([0.1,1,1E7,1E8,50])
     #param_guess = ca.DM([0.001,0.009,1,1E6,1E7,1])
     #param_est = ParameterEstimation(cfg_path, y_data, param_guess)
-    
+
     #    with ParameterEstimation(config=cfg_path,
     #                             data=y_data,
     #                             param_guess=param_guess) as param_est:
-    
+
     with ParameterEstimation(config=cfg_path,
                              N=N,
                              dt=dt,
                              param_guess=param_guess) as param_est:
-        
+
         Q = ca.DM.eye(2)
         #Q[0,0] = 6.55919266e-01
         #Q[1,1] = 4.52496393e-01
@@ -101,28 +101,28 @@ if __name__ == "__main__":
         # provide lb, ub for p here:
         #grad_f = ca.gradient(param_est.nlp["f"], param_est.nlp["x"])
         #grad_f_f = ca.Function("grad_f", [param_est.nlp["x"]], [grad_f])
-        
+
         """
         from cvxpy with first solution:
         [[6.55919266e-01 5.26466051e-12]
         [5.26466051e-12 4.52496393e-01]]
-        
+
         params after first solve:
         Rie    1.532625e-03
         Rea    1.160423e-02
         Ci     1.328633e+06
         Ce     6.644898e+06
         Ai     5.578587e+00
-        
+
         params after second solve:
         Rie    1.567088e-03
         Rea    1.160390e-02
         Ci     1.337609e+06
         Ce     6.747752e+06
         Ai     5.132032e+00m here we k
-                        
-        Does it change the physical parameters any???    
-            
+
+        Does it change the physical parameters any???
+
         """
         sol, params = param_est.solve(
                                       y_data,
@@ -131,10 +131,10 @@ if __name__ == "__main__":
                                       ubp=ubp,
                                       covar=ca.veccat(Q, R)
                                       )
-    
+
         ax = sol["Ti"].plot(color="r")
         sol["y1"].plot(color="k", ax=ax)
         ax.legend()
         plt.show()
         print(params)
-        
+
