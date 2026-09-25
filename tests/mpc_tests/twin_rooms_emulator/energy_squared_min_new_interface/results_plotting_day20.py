6b8c023991f07e17fd4060c7437d274bd45c251d
Marius Bagle <marius@NTNU24923>
2026-09-25 13:49:28 +0200

    latest

--- tests/mpc_tests/twin_rooms_emulator/energy_squared_min_new_interface/results_plotting_day20.py
+++ tests/mpc_tests/twin_rooms_emulator/energy_squared_min_new_interface/results_plotting_day20.py
@@ -18,9 +18,9 @@ from ocp.tests.mpc_tests.twin_rooms_emulator.energy_squared_min_new_interface.ut
     one_step_cost_pred, \
     get_value_function_error_all_N
 import matplotlib.dates as mdates
-from matplotlib.ticker import MaxNLocator
+from matplotlib.ticker import MaxNLocator, ScalarFormatter, FuncFormatter
 from pathlib import Path
-from operating_point_plot import plot_operating_context
+from operating_point_plot import plot_operating_context, CONTROLLER_NAMES
 
 rc('mathtext', default='regular')
 
@@ -32,12 +32,12 @@ if __name__ == "__main__":
     base = Config()("base_config_scaled.json")
     #base["days"] = 28
     #base["days"] = 31 + 28 + 31 + 30 + 31 
-    #base["days"] = 365
-    base["days"] = 28
+    base["days"] = 365
+    #base["days"] = 28
     #base["days"] = 2
     stop_day = 182
     #stop_day = 14
-    stop_day = 28
+    #stop_day = 28
 
     starts = {
         "adaptive_cost_free_rad": pd.Timedelta(days=8),
@@ -171,6 +171,8 @@ if __name__ == "__main__":
 
     almost_one = active_diff[active_diff > 0.8]
     hard_6h = active_diff[active_diff > 0.4]
+    # need this:
+    p = coord.controller.p
 
     """
     fig, axes = plt.subplots(2,1, sharex=True)
@@ -181,6 +183,7 @@ if __name__ == "__main__":
     plt.show()
     """
     
+    """
     # 'true' difference:
     exact_time = pd.Timedelta(days=20, hours=11, minutes=45)  
     # same as:
@@ -189,7 +192,6 @@ if __name__ == "__main__":
     # morning anticipation:
     #int_ndx = coord.res.index.get_loc(exact_time)
     int_ndx = control_orig.index.get_loc(exact_time)
-    p = coord.controller.p
     for k, v in read_coords.items():
         print(k)
         pred = v.controller.preds[int_ndx]
@@ -213,13 +215,14 @@ if __name__ == "__main__":
         #operating_point=operating_point,
         #operating_point=None,
         sampling="15min",
-        filename="OP22.pdf",
+        filename="OP20.pdf",
     )
     plt.show(block=False)
 
     """
 
-    exact_time = candidates.index[2]
+    exact_time = candidates.index[1]
+    int_ndx = control_orig.index.get_loc(exact_time)
     start = pd.Timedelta(days=35)
     #start = pd.Timedelta(days=48)
     stop = start + pd.Timedelta(days=3)
@@ -235,6 +238,7 @@ if __name__ == "__main__":
     )
     plt.show(block=False)
     
+    """
     fig, axess = plt.subplots(6,1, sharex=True)
     for i, (k, v) in enumerate(read_coords.items()):
         _start = 2*i
@@ -248,10 +252,21 @@ if __name__ == "__main__":
         #fig.suptitle(k)
     plt.show(block=False)
     print("head")
+    """ 
     
-    """
-    
-    def sweep_params(first, second, coord, int_ndx, ax, title, alpha=1.0, H=1, state=False):
+    def sweep_params(
+        first,
+        second,
+        coord,
+        int_ndx,
+        ax,
+        title,
+        alpha=1.0,
+        H=1,
+        state=False,
+        first_label="",
+        second_label="",
+    ):
 
         us = pd.DataFrame()
         u_primes = pd.DataFrame()
@@ -329,6 +344,11 @@ if __name__ == "__main__":
 
         first_u_vals = us.index.to_numpy()
         second_u_vals = us.columns.to_numpy()
+        
+        if state:
+            first_u_vals = first_u_vals - 273.15
+            second_u_vals = second_u_vals - 273.15
+        
 
         first_grid, second_grid = np.meshgrid(
             first_u_vals, second_u_vals
@@ -349,8 +369,8 @@ if __name__ == "__main__":
             first_grid,
             second_grid,
             U_plot,
-            #levels=np.linspace(0, 1, 11),
-            levels=levels,
+            levels=np.linspace(0, 1, 11),
+            #levels=levels,
             cmap="viridis",
             vmin=0,
             vmax=1
@@ -369,6 +389,10 @@ if __name__ == "__main__":
         values = coord.controller.preds[int_ndx].loc[
             0, [first["name"], second["name"]]
         ].values
+        
+        if state:
+            values -= 273.15
+        
         #eps = 0.1
         print(title + ":")
         print(first["name"] + ": " + str(values[0]))
@@ -377,6 +401,8 @@ if __name__ == "__main__":
             values[0],
             values[1],
             #u_stars[orig] + eps,
+            edgecolor="white",
+            linewidth=1.0,
             s=30,
             marker="o",
             color="black",
@@ -386,14 +412,18 @@ if __name__ == "__main__":
 
         #ax.set_xlabel(r"$C_{e}$")
         #ax.set_ylabel(r"$C_{i}$")
-        ax.set_xlabel(first["label"])
-        ax.set_ylabel(second["label"])
+        ax.set_xlabel(
+            first_label
+        )
+        ax.set_ylabel(
+            second_label
+        )
         ax.set_title(title)
         #ax.set_zlabel(r"$u^\star_{\mathrm{rad}}$")
 
         #fig.colorbar(surf, ax=ax, label=r"$u^\star_{\mathrm{rad}}$")
-        fig.colorbar(cf, ax=ax, label=r"$u^\star_{\mathrm{rad}}$")
-        return us, u_primes
+        #fig.colorbar(cf, ax=ax, label=r"$u^\star_{\mathrm{rad}}$")
+        return us, u_primes, cf
 
     ces = np.arange(0.1e7,1.1e8,1e7)
     cis = np.arange(0.1e6,1.1e7,1e6)
@@ -407,35 +437,156 @@ if __name__ == "__main__":
     #cis = np.arange(1e7,1e8,1e7)
     #Tes = np.arange(292.15,294.35,0.2)
     #Tis = np.arange(293.15,295.35,0.2)
+    #Tes = np.arange(290.15,292.35,0.2)
+    #Tis = np.arange(290.15,292.35,0.2)
     
+    """
     obs = read_coords[
         "baseline_cost_hist"
     ].controller.preds[int_ndx][
         ["Te", "Ti" ]
     ].iloc[0].values
     
+    Cs = read_coords[
+        "baseline_cost_hist"
+    ].controller.preds[int_ndx][
+        ["Ce", "Ci" ]
+    ].iloc[0].values
+    """
+    
+    obs_all = pd.DataFrame(
+        index=list(read_coords.keys()),
+        columns=["Te", "Ti"]
+    )
+    
+    for k, v in read_coords.items():
+        obs_all.loc[k, ["Te", "Ti"]] = v.controller.preds[int_ndx][
+            ["Te", "Ti" ]
+        ].iloc[0]
+    
+    obs = obs_all.mean().values
+    
+    
+    Cs_all = pd.DataFrame(
+        index=list(read_coords.keys()),
+        columns=["Ce", "Ci"]
+    )
+
+    for k, v in read_coords.items():
+        Cs_all.loc[k, ["Ce", "Ci"]] = v.controller.preds[int_ndx][
+            ["Ce", "Ci" ]
+        ].iloc[0]
+    
+    Cs = Cs_all.mean().values
+    
+    Rs_all = pd.DataFrame(
+        index=list(read_coords.keys()),
+        columns=["Rea", "Rie"]
+    )
+
+    for k, v in read_coords.items():
+        Rs_all.loc[k, ["Rea", "Rie"]] = v.controller.preds[int_ndx][
+            ["Rea", "Rie" ]
+        ].iloc[0]
+    
+    Rs = Rs_all.mean().values
+    
     #Tes = np.arange(290.15,292.35,0.2)
     #Tis = np.arange(290.15,292.35,0.2)
     
-    delta = 0.25
+    # (Ti, Te):
+    
+    delta = 0.35
+    n_segments = 7
     Tes = np.concatenate(
         [
-         np.arange(obs[0] - delta, obs[0], 0.05),
-         np.arange(obs[0], obs[0] + delta, 0.05)
+         np.arange(obs[0] - delta, obs[0], delta/n_segments),
+         np.arange(obs[0], obs[0] + delta, delta/n_segments)
         ]
     )
     Tis = np.concatenate(
         [
-         np.arange(obs[1] - delta, obs[1], 0.05),
-         np.arange(obs[1], obs[1] + delta, 0.05)
+         np.arange(obs[1] - delta, obs[1], delta/n_segments),
+         np.arange(obs[1], obs[1] + delta, delta/n_segments)
+        ]
+    )
+
+    # (Ci, Ce):
+
+    delta = 2e7
+    n_segments = 5
+    ces = np.concatenate(
+        [
+         np.arange(Cs[0] - delta, Cs[0], delta/n_segments),
+         np.arange(Cs[0], Cs[0] + delta, delta/n_segments)
+        ]
+    )
+    delta = 2e6
+    cis = np.concatenate(
+        [
+         np.arange(Cs[1] - delta, Cs[1], delta/n_segments),
+         np.arange(Cs[1], Cs[1] + delta, delta/n_segments)
         ]
     )
     
-    Prads = np.arange(1500,2500,100)
+    # (Rie, Rea):
+
+    delta = 1e-2
+    n_segments = 10
+    reas = np.concatenate(
+        [
+         np.arange(Rs[0] - delta, Rs[0], delta/n_segments),
+         np.arange(Rs[0], Rs[0] + delta, delta/n_segments)
+        ]
+    )
+    delta = 1e-3
+    ries = np.concatenate(
+        [
+         np.arange(Rs[1] - delta, Rs[1], delta/n_segments),
+         np.arange(Rs[1], Rs[1] + delta, delta/n_segments)
+        ]
+    )
+
+    # test version:
+    Prads = [1500, 2000]
+    ns = [1,2]
+    
+    #cis = [1e6,1e7]
+    #ces = [1e7,1e8]
+    
+    #Tis = [290,295]
+    #Tes = [290,295]
+    
+    reas = [1e-2,2e-2]
+    ries = [1e-3,2e-3]
+    
+    ###
+    
+    reas = [1e-2,2e-2,3e-2,4e-2,5e-2,6e-2,7e-2,8e-2,9e-2,1e-1,1.1e-1,1.2e-1,1.3e-1]
+    ries = [1e-3,2e-3,3e-3,4e-3,5e-3,6e-3,7e-3,8e-3,9e-3,1e-2,1.1e-2,1.2e-2]
+    
+    Prads = np.arange(1500,2600,100)
     ns = np.arange(0.1,3.1,0.3)
-
-
-    ### Ci, Ce:
+    
+    #Tes = np.arange(290.15,292.35,0.2)
+    #Tis = np.arange(290.15,292.35,0.2)
+    
+    #ces = np.arange(0.1e7,1.1e8,1e7)
+    #cis = np.arange(0.1e6,1.1e7,1e6)
+    
+    """
+    H-analysis:
+    """
+    first = {
+        "values": reas,
+        "name": "Rea",
+        "label": "$R_{ea}$"
+    }
+    second = {
+        "values": ries,
+        "name": "Rie",
+        "label": "$R_{ie$"
+    }
     
     first = {
         "values": ces,
@@ -448,19 +599,19 @@ if __name__ == "__main__":
         "label": "$C_i$"
     }
     
-    """
+    
     # Resampling open-loop planning:
-    fig, axes = plt.subplots(5,3, figsize=(18, 5))
+    fig, axes = plt.subplots(4,3, figsize=(18, 5))
     #_fig, _axes = plt.subplots(1,3, figsize=(18, 5))
     
-    for i, H in enumerate((1,2,4,8,32)):
+    for i, H in enumerate((1,2,4,8)):
     #for i, H in enumerate((1,8)):
     
         us = {}
         u_primes = {}
         for j, (k, v) in enumerate(read_coords.items()):
             # first, second, coord, int_ndx, ax, title
-            us[k], u_primes[k] = sweep_params(
+            us[k], u_primes[k], _ = sweep_params(
                 first, 
                 second,
                 v, 
@@ -473,115 +624,197 @@ if __name__ == "__main__":
     plt.show(block=False)
     print("tail")
     
-    """
     
-    """
-    ### Prad, n:
+    all_params ={
+        "Cs": {
+            "first": {
+                "values": ces,
+                "name": "Ce",
+                #"label": "$C_e \\, [10^{8} \\frac{\\text{J}}{\\text{K}}]$"
+                "label": "$C_e \\, [10^{8} \\, \\mathrm{J/K}]$"
+            },
+            "second": {
+                "values": cis,
+                "name": "Ci",
+                "label": "$C_i \\, [10^{7} \\, \\mathrm{J/K}]$"
+            }
+        },
+        "Rs": {
+            "first": {
+                "values": reas,
+                "name": "Rea",
+                "label": "$R_{ea} \\, [10^{-2} \\, \\mathrm{K/W}]$"                
+            },
+            "second": {
+                "values": ries,
+                "name": "Rie",
+                "label": "$R_{ie} \\, [10^{-3} \\, \\mathrm{K/W}]$"                    
+            }
+        },
+        "Ts": {
+            "first": {
+                "values": Tes,
+                "name": "Te",
+                "label": "$T_e \\, [^\\circ\\mathrm{C}]$"
+            },
+            "second": {
+                "values": Tis,
+                "name": "Ti",
+                "label": "$T_i \\, [^\\circ\\mathrm{C}]$"
+            }
+        },
+        "Prad": {
+            "first": {
+                "values": Prads,
+                "name": "Prad_nom",
+                "label": "$P_{rad}  \\, [\\mathrm{W}]$"
+            },
+            "second": {
+                "values": ns,
+                "name": "n",
+                "label": "$n  \\, [-]$"                
+            }
+        }
+    }
+    
+    fig, axes = plt.subplots(4,3, figsize=(10, 6.5))
+    us = {}
+    u_primes = {}
+    for i, (_k, pairs) in enumerate(all_params.items()):
+        for j, (k, v) in enumerate(read_coords.items()):
+            #if k == "baseline_cost_hist":
+            if True:
+                if i == 0:
+                    title = CONTROLLER_NAMES[k]
+                else:
+                    title = ""
+                if j == 0:
+                    first_label = pairs["first"]["label"]
+                    second_label = pairs["second"]["label"]
+                else:
+                    first_label = ""
+                    second_label = ""
+                    
+                # first, second, coord, int_ndx, ax, title
+                if _k == "Ts":
+                    state = True
+                else:
+                    state = False
+                us[k], u_primes[k], cf = sweep_params(
+                    pairs["first"], 
+                    pairs["second"],
+                    v, 
+                    int_ndx,
+                    axes[i,j],
+                    title,
+                    state=state,
+                    first_label=first_label,
+                    second_label=second_label
+                )
+    # colorbar here:
+    #fig.colorbar(cf, ax=ax, label=r"$u^\star_{\mathrm{rad}}$")          
+    cbar = fig.colorbar(
+        cf,
+        ax=axes,
+        location="right",
+        fraction=0.025,
+        pad=0.02,
+    )
+    cbar.set_ticks([0, 0.5, 1.0])
+    cbar.set_label(r"$u_{\mathrm{val}}^\star$ [-]")
+    
+    # ticks:
+    # Ci, Ce
+    for ax in axes[0, :]:
+        #ax.set_xticks([0.2e8, 0.6e8, 1.0e8])
+        ax.set_yticks([0.2e7, 0.4e7])
+        ax.set_xticks([0.1e8, 0.4e8])
+        #ax.set_xticks([round(ces[0], 8), round(ces[-1], 8)])
+        #ax.set_yticks([0.2e7, 0.6e7, 1.0e7])
+        #ax.set_xticks([round(cis[0], 7), round(cis[-1], 7)])
+        ax.xaxis.set_major_formatter(
+            FuncFormatter(lambda x, pos: f"{x/1e8:g}")
+        )
+        ax.yaxis.set_major_formatter(
+            FuncFormatter(lambda y, pos: f"{y/1e7:g}")
+        )
 
-    first = {
-        "values": Prads,
-        "name": "Prad_nom",
-        "label": "$P_{rad}$"
-    }
-    second = {
-        "values": ns,
-        "name": "n",
-        "label": "$n$"
-    }
+    # Rie, Rea
+    for ax in axes[1, :]:
+        ax.set_xticks([0.02, 0.13])
+        
+        """
+        fmt_x = ScalarFormatter(useMathText=True)
+        fmt_x.set_powerlimits((-2, -2))
+        ax.xaxis.set_major_formatter(fmt_x)
+        """
+        
+        ax.set_yticks([0.002, 0.01])
+        
+        """
+        fmt_y = ScalarFormatter(useMathText=True)
+        fmt_y.set_powerlimits((-3, -3))
+        ax.yaxis.set_major_formatter(fmt_y)
+        """
+        ax.xaxis.set_major_formatter(
+            FuncFormatter(lambda x, pos: f"{x/1e-2:g}")
+        )
+        # R_ea: display 0.001 as 1, etc.
+        ax.yaxis.set_major_formatter(
+            FuncFormatter(lambda y, pos: f"{y/1e-3:g}")
+        )
     
-    fig, axes = plt.subplots(7,3, figsize=(18, 5))
-    #for j, alpha in enumerate((0, 1, 2, 4, 8)):
-    #for j, alpha in enumerate((1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8)):
-    #for j, alpha in enumerate((0.0, 0.2, 0.4, 0.6, 0.8, 1.0)):
-    #for j, alpha in enumerate((0.8,0.84,0.88,0.92,0.96,1.0)):
-    for j, alpha in enumerate((0.88,0.89,0.90,0.91,0.92,0.93)):
-    #for j, alpha in enumerate((0.91,0.92,0.93,0.94,0.95,0.96,0.97)):
-    #for j, alpha in enumerate((0.97,0.98,0.99,1.0,1.01,1.02)):
-        #fig, axes = plt.subplots(1,3, figsize=(18, 5))
-        us = {}
-        u_primes = {}
-        for i, (k, v) in enumerate(read_coords.items()):
-            # first, second, coord, int_ndx, ax, title
-            us[k], u_primes[k] = sweep_params(
-                first, 
-                second,
-                v, 
-                int_ndx,
-                axes[j,i],
-                k,
-                alpha=alpha,
-                state=False
+    # Te, Ti
+    for ax in axes[2, :]:
+        #ax.set_xticks(np.array([292.15]) - 273.15)
+        #ax.set_yticks(np.array([290.15, 292.15]) - 273.15)
+        ax.set_yticks(
+            np.array(
+                [
+                    round(Tis[0] - 273.15, 1) + 0.1,
+                    round(Tis[-1] - 273.15, 1) - 0.1
+                ]
             )
-        alpha_str = str(alpha)
-        axes[j,1].set_title("alpha=" + alpha_str)
-        print("tail")
-    plt.show(block=False)
-    """
-
-    ### Rie, Rea:
-
-    first = {
-        "values": reas,
-        "name": "Rea",
-        "label": "$R_{ea}$"
-    }
-    second = {
-        "values": ries,
-        "name": "Rie",
-        "label": "$R_{ie}$"
-    }
-    
-    fig, axes = plt.subplots(1,3, figsize=(18, 5))
-    us = {}
-    u_primes = {}
-    for i, (k, v) in enumerate(read_coords.items()):
-        # first, second, coord, int_ndx, ax, title
-        us[k], u_primes[k] = sweep_params(
-            first, 
-            second,
-            v, 
-            int_ndx,
-            axes[i],
-            k,
-            state=False
         )
-    plt.show(block=False)
-    print("tail")
-    
-    
-    ### Ti, Te:
-    
-    first = {
-        "values": Tes,
-        "name": "Te",
-        "label": "$T_e$"
-    }
-    second = {
-        "values": Tis,
-        "name": "Ti",
-        "label": "$T_i$"
-    }
-    fig, axes = plt.subplots(1,3, figsize=(18, 5))
-    us = {}
-    u_primes = {}
-    for i, (k, v) in enumerate(read_coords.items()):
-        # first, second, coord, int_ndx, ax, title
-        us[k], u_primes[k] = sweep_params(
-            first, 
-            second,
-            v,
-            int_ndx,
-            axes[i],
-            k,
-            state=True
+        ax.set_xticks(
+            np.array(
+                [
+                    round(Tes[0] - 273.15, 1) + 0.1,
+                    round(Tes[-1] - 273.15, 1) - 0.1
+                ]
+            )
         )
-    plt.show(block=False)
-    print("tail")
+
+    # Prad, n
+    for ax in axes[3, :]:
+        ax.set_xticks([1500, 2500])
+        
+    fig.subplots_adjust(
+        left=0.10,
+        right=0.86,
+        bottom=0.10,
+        top=0.92,
+        wspace=0.22,
+        hspace=0.34,
+    )
     
+    #for ax in axes.flat:
+    #    ax.xaxis.labelpad = -1
+    for ax in axes.flat:
+        ax.xaxis.set_label_coords(0.5, -0.08)
+    
+    plt.show(block=False)
+    fig.savefig(
+        #"PS20.pdf",
+        "PS36.pdf",
+        bbox_inches="tight",
+    )
+    print("tail")
     
     u_stars = {}
     for k, v in read_coords.items():
         u_stars[k] = v.res.loc[exact_time + pd.Timedelta(minutes=15), "rad_219"]
+        #u_stars[k] = v.res.loc[exact_time, "rad_219"]
 
     print("mid")
     
